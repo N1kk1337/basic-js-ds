b203a656583c63e8a4239b6ab287fe95cca1c6c5
Nikki NoLife <nikkinolife@gmail.com>
2022-10-18 02:27:17 +0400

    feat queue

--- src/queue.js
+++ src/queue.js
@@ -15,22 +15,41 @@ const { NotImplementedError } = require("../extensions/index.js");
  */
 class Queue {
   constructor() {
-    // this.start = new ListNode();
     this.start = null;
+    this.end = null;
   }
 
   getUnderlyingList() {
-    return { value: this.arr[0], next: this.arr[1] };
+    return this.start;
   }
 
   enqueue(value) {
     //this.arr.unshift(value);
+    let newNode = new ListNode(value);
+
+    if (this.end === null) {
+      this.start = newNode;
+      this.end = newNode;
+    } else {
+      this.end.next = newNode;
+      this.end = newNode;
+    }
   }
 
   dequeue() {
     //let last = this.arr[this.arr.length - 1];
 
-    return this.arr.pop();
+    if (this.start == null) return null;
+
+    let tempNode = this.start;
+    this.start = this.start.next;
+
+    if (this.start == null) {
+      this.end = null;
+    }
+
+    //return this.arr.pop();
+    return tempNode.value;
   }
 }
 
